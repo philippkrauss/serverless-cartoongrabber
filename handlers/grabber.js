aef2074860a95f8a44d937b513a616e6ff3e6da5
Philipp Krauss <philipp.krauss@codecentric.de>
2020-05-08 16:46:52 +0200

    add dynamodb storage

--- handlers/grabber.js
+++ handlers/grabber.js
@@ -1,25 +1,52 @@
 const axios = require('axios')
+const dynamodb = require('serverless-dynamodb-client');
+
+const docClient = dynamodb.doc;
+
+//TODO logging?
+module.exports.grab = async (event, context, cb) => {
+    try {
+        const response = await axios.get('https://www.ruthe.de')
+        const url = extractImageUrlFromMeta(response.data)
+        // await addCartoonToDb({
+        //     name: 'ruthe',
+        //     lastImageUrl: url,
+        // })
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                url: url,
+            })
+        }
+    } catch (error) {
+        console.error(error)
+        return {
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                error: error
+            })
+        }
+    }
+}
+
+async function addCartoonToDb(cartoon) {
+    console.log('Submitting cartoon');
+    const tableName = process.env.CARTOON_TABLE ? process.env.CARTOON_TABLE : 'cartoons'
+    return await docClient.put({
+        TableName: tableName,
+        Item: cartoon,
+    }).promise()
+}
 
 function extractImageUrlFromMeta(data) {
     let matched = data.match(/<meta property="og:image" content="(.*)">/)
     if (matched && matched.length > 1) {
         return matched[1]
     }
-}
-
-module.exports.grab = async (event, context, cb) => {
-    let responseBody
-    try {
-        const response = await axios.get('https://www.ruthe.de')
-        responseBody = extractImageUrlFromMeta(response.data)
-    } catch (error) {
-        responseBody = 'nothing found'
-    }
-    cb(null, {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'text/plain'
-        },
-        body: responseBody
-    })
 }
--- test/grabber.test.js
+++ test/grabber.test.js
@@ -3,15 +3,14 @@ const axios = require('axios')
 
 jest.mock('axios')
 
-test('grab', done => {
+test('grab', async () => {
     const HTML_CONTENT = '<meta property="og:image" content="some-cartoon-url.jpg">'
     const axiosGetStub = axios.get.mockResolvedValue({data: HTML_CONTENT})
-    grabber.grab(null, null, (err, response) => {
-        expect(response.body).toBe('some-cartoon-url.jpg')
-        expect(axiosGetStub.mock.calls[0][0]).toEqual(
-			'https://www.ruthe.de'
-		)
-        done()
-    })
+
+    const response = await grabber.grab(null, null)
+    expect(response.body).toBe("{\"url\":\"some-cartoon-url.jpg\"}")
+    expect(axiosGetStub.mock.calls[0][0]).toEqual(
+        'https://www.ruthe.de'
+    )
 })
 
