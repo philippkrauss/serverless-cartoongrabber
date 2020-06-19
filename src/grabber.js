eb2b8b51b14d577c0676343c2e788c2bd2deaaf1
Philipp Krauss <philipp.krauss@codecentric.de>
2020-06-19 15:33:22 +0200

    Implement additional cartoon sources

--- src/grabber.js
+++ src/grabber.js
@@ -1,28 +1,68 @@
 const axios = require('axios')
-const response = require('./response')
 const dynamoDbClient = require('./dynamoDbClient')
 
+const SOURCES = [
+	{
+		name: 'Ruthe',
+		url: 'https://www.ruthe.de/',
+		grabber: grabUsingMetaProperty,
+	},
+	{
+		name: 'Zits',
+		url: 'https://www.comicskingdom.com/zits/',
+		grabber: grabUsingMetaProperty,
+	},
+	{
+		name: 'Dilbert',
+		url: 'https://dilbert.com/strip/',
+		grabber: grabUsingDateAndMetaProperty,
+	},
+]
+module.exports.SOURCES = SOURCES
 /* eslint-disable no-unused-vars */
 module.exports.grab = async (event, context) => {
 	/* eslint-enable */
-	const source = {
-		url: 'https://www.ruthe.de',
-		name: 'ruthe',
+	for (const source of SOURCES) {
+		try {
+			const cartoon = await source.grabber({
+				name: source.name,
+				url: source.url,
+			})
+			if (cartoon) {
+				await addCartoonToDb(cartoon)
+			}
+		} catch (error) {
+			console.error('an unexpected error occurred', error)
+		}
 	}
+}
+
+async function grabUsingMetaProperty(source) {
 	try {
+		console.log('grabbing using meta property, source: ', source.name)
 		const websiteText = await getWebsiteText(source.url)
 		const url = extractImageUrlFromMeta(websiteText)
-		const cartoon = createCartoon(source.name, url)
-		await addCartoonToDb(cartoon)
-		return response.createResponse({
-			url: url,
-		})
+		return createCartoon(source.name, url)
 	} catch (error) {
-		console.error('an error occurred', error)
-		return response.createErrorResponse({
-			error: error,
-		})
+		console.error('an error occurred when grabbing from ' + source.name, error)
 	}
+}
+module.exports.grabUsingMetaProperty = grabUsingMetaProperty
+
+async function grabUsingDateAndMetaProperty(source) {
+	try {
+		console.log('grabbing using date and meta property, source: ', source.name)
+		const newUrl = source.url + formatCurrentDate()
+		return grabUsingMetaProperty({ name: source.name, url: newUrl })
+	} catch (error) {
+		console.error('an error occurred when grabbing from ' + source.name, error)
+	}
+}
+module.exports.grabUsingDateAndMetaProperty = grabUsingDateAndMetaProperty
+
+function formatCurrentDate() {
+	const currentDate = new Date()
+	return currentDate.toISOString().substr(0, 10)
 }
 
 async function getWebsiteText(url) {
@@ -40,7 +80,7 @@ async function addCartoonToDb(cartoon) {
 }
 
 function extractImageUrlFromMeta(data) {
-	let matched = data.match(/<meta property="og:image" content="(.*)">/)
+	let matched = data.match(/<meta property="og:image" content="(.*)"/)
 	if (matched && matched.length > 1) {
 		console.log('extracted ', matched[1])
 		return matched[1]
--- test/grabber.test.js
+++ test/grabber.test.js
@@ -1,49 +1,45 @@
 const grabber = require('../src/grabber')
 const axios = require('axios')
-const dynamoDbClient = require('../src/dynamoDbClient')
+const MockDate = require('mockdate')
 
 jest.mock('axios')
-jest.mock('../src/dynamoDbClient')
 
 const CARTOON_URL = 'some-cartoon-url.jpg'
 const HTML_CONTENT = `<meta property="og:image" content="${CARTOON_URL}">`
+const SOURCE = {
+	name: 'ruthe',
+	url: 'https://www.ruthe.de/',
+}
 
-test('grab a cartoon and store it into dynamodb', async () => {
+test('grab a cartoon using meta property', async () => {
 	const axiosGetStub = axios.get.mockResolvedValue({ data: HTML_CONTENT })
-	const dynamoDbPutStub = dynamoDbClient.put.mockResolvedValue({})
-	const response = await grabber.grab(null, null)
-	expect(response.statusCode).toEqual(200)
+	const cartoon = await grabber.grabUsingMetaProperty(SOURCE)
 	expect(axiosGetStub).toHaveBeenCalledTimes(1)
-	expect(axiosGetStub).toHaveBeenCalledWith('https://www.ruthe.de')
-	expect(dynamoDbPutStub).toHaveBeenCalledTimes(1)
-	expect(dynamoDbPutStub).toHaveBeenCalledWith({
-		tableName: 'cartoons',
-		item: {
-			lastImageUrl: CARTOON_URL,
-			name: 'ruthe',
-		},
+	expect(axiosGetStub).toHaveBeenCalledWith('https://www.ruthe.de/')
+	expect(cartoon).toEqual({
+		lastImageUrl: CARTOON_URL,
+		name: 'ruthe',
 	})
 })
-test('HTTP error when grabbing', async () => {
+test('HTTP error when grabbing using meta property', async () => {
 	axios.get.mockRejectedValue(new Error())
-	const response = await grabber.grab(null, null)
-	expect(response.statusCode).toEqual(500)
+	const cartoon = await grabber.grabUsingMetaProperty(SOURCE)
+	expect(cartoon).toBeUndefined()
 })
-test('bad HTML returned when grabbing', async () => {
+test('bad HTML returned when grabbing using meta property', async () => {
 	axios.get.mockResolvedValue({ data: 'this does not match!' })
-	const response = await grabber.grab(null, null)
-	expect(response.statusCode).toEqual(500)
+	const cartoon = await grabber.grabUsingMetaProperty(SOURCE)
+	expect(cartoon).toBeUndefined()
 })
-test('bad HTML returned when grabbing', async () => {
-	axios.get.mockResolvedValue({ data: 'this does not match!' })
-	const response = await grabber.grab(null, null)
-	expect(response.statusCode).toEqual(500)
-})
-test('dynamodb error when grabbing', async () => {
+
+test('grab a cartoon using date and meta property', async () => {
+	MockDate.set('2020-06-01')
 	const axiosGetStub = axios.get.mockResolvedValue({ data: HTML_CONTENT })
-	const dynamoDbPutStub = dynamoDbClient.put.mockRejectedValue({})
-	const response = await grabber.grab(null, null)
-	expect(response.statusCode).toEqual(500)
+	const cartoon = await grabber.grabUsingDateAndMetaProperty(SOURCE)
 	expect(axiosGetStub).toHaveBeenCalledTimes(1)
-	expect(dynamoDbPutStub).toHaveBeenCalledTimes(1)
+	expect(axiosGetStub).toHaveBeenCalledWith('https://www.ruthe.de/2020-06-01')
+	expect(cartoon).toEqual({
+		lastImageUrl: CARTOON_URL,
+		name: 'ruthe',
+	})
 })
