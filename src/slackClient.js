6a2da1a9f5c3ce391f50c1d2015f8d4515f9c6cc
Philipp Krauss <philipp.krauss@codecentric.de>
2020-06-24 08:35:38 +0200

    switch slack integration to webhook

--- src/slackClient.js
+++ src/slackClient.js
@@ -1,24 +1,15 @@
 const axios = require('axios')
 
-const slackToken = process.env.SLACK_TOKEN
+const slackUrl = process.env.SLACK_URL
 const slackChannelName = process.env.SLACK_CHANNEL_NAME
 
 async function sendMessage(text) {
-	const config = {
-		headers: {
-			Authorization: 'Bearer ' + slackToken,
-		},
-	}
 	const body = {
 		channel: slackChannelName,
 		text,
 	}
 	console.log('sending to slack: ', body)
-	let response = await axios.post(
-		'https://slack.com/api/chat.postMessage',
-		body,
-		config
-	)
+	let response = await axios.post(slackUrl, body)
 	console.log('slack response: ', response.data)
 }
 
